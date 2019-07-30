1e48783aff0b6f2d57fef165a5cae4519bb314bf
kiki-ki <malpie0619@gmail.com>
2019-07-30 20:09:51 +0900

    Fix friends

--- git_workshop/intro.rb
+++ git_workshop/intro.rb
@@ -13,16 +13,16 @@ intros = "Hello, I'm Taro.\n"
 
 case kind
 when "account"
-  FILE.open(path) do |f|
+  File.open(path) do |f|
     account_num = f.read
     intros += "my account num is #{account_num}"
   end
   puts intros
 when "friends"
   intros += "my friends are\n"
-  FILE.open(path) do |f|
+  File.open(path) do |f|
     friends = f.read
-    intros += frends
+    intros += friends
   end
   puts intros
 else
