b2d7737616c4be9cf4e155864fa6ea9852000b52
nakata kai <malpie0619@gmail.com>
2020-04-11 10:46:28 +0900

    05

---
+++ aHundredDrill2020/01_warming_up/05.rb
@@ -0,0 +1,6 @@
+def to_ngram(v)
+  v = v.delete(" ").split("") if v.instance_of?(String)
+  v.map.with_index{ |w, i| "#{w}#{v[i + 1]}" }
+end
+p to_ngram(%W(I am an NLPer))
+p to_ngram("I am an NLPer")
