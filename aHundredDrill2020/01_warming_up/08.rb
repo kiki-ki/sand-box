bd2c779c050fe7210a0208c1af96f95ec0a03eb6
nakata kai <malpie0619@gmail.com>
2020-04-12 01:37:01 +0900

    modify 08

--- aHundredDrill2020/01_warming_up/08.rb
+++ aHundredDrill2020/01_warming_up/08.rb
@@ -1,6 +1,6 @@
 def cipher(s)
-  range = ("a".."z")
-  s.length.times.map{ |i| range.include?(s[i]) ? (219 - s[i].ord).chr : s[i] }.join
+  tar = ("a".."z")
+  s.length.times.map{ |i| tar.include?(s[i]) ? (219 - s[i].ord).chr : s[i] }.join
 end
 p s = "abcABC"
 p encoded = cipher(s)
