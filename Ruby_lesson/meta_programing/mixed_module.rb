72e82782ba3d4913b67bb67211e74b0bc09110a5
nakata kai <malpie0619@gmail.com>
2020-04-18 13:21:40 +0900

    running

---
+++ Ruby_lesson/meta_programing/mixed_module.rb
@@ -0,0 +1,30 @@
+module Printable
+  def print
+    p "print out."
+  end
+
+  def prepare_cover
+    p "covered."
+  end
+end
+
+module Document
+  def print_to_screen
+    prepare_cover
+    format_for_screen
+    print
+  end
+
+  def format_for_screen
+    p "formated."
+  end
+
+  def print
+    p "in the Document."
+  end
+end
+
+class Book
+  prepend Document
+  include Printable
+end
