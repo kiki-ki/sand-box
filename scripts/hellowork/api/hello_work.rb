6467a99dc8ad1a7db518aa2a52c3dfb9817e2164
nakata kai <malpie0619@gmail.com>
2020-07-10 17:38:04 +0900

    create hello_work.rb

---
+++ scripts/hellowork/api/hello_work.rb
@@ -0,0 +1,58 @@
+require 'yaml'
+require 'rest-client'
+require 'active_support'
+require 'active_support/core_ext'
+
+class User
+  attr_reader *%i(id pass token)
+
+  def initialize
+    info = YAML.load_file("config/secret.yml")["hello_work"]
+    @id = info["id"]
+    @pass = info["pass"]
+  end
+
+  def token
+    @token ||= get_token
+  end
+
+  def destroy_token
+    res = RestClient.post(
+      'https://teikyo.hellowork.mhlw.go.jp/teikyo/api/2.0/auth/delToken',
+      token
+    )
+    self.token = nil
+  end
+
+  private
+    def get_token
+      res = RestClient.post(
+        'https://teikyo.hellowork.mhlw.go.jp/teikyo/api/2.0/auth/getToken',
+        { id: id, pass: pass }
+      )
+      Hash.from_xml(res.body)["root"]["token"]
+    end
+end
+
+class ApiClient
+  def initialize
+    u = User.new
+    @token = u.token
+  end
+
+  def get_kyujin_list
+    res = RestClient.post(
+      'https://teikyo.hellowork.mhlw.go.jp/teikyo/api/2.0/kyujin',
+      { token: @token }
+    )
+    Hash.from_xml(res.body)["root"]["kyujin_list"]["data"]
+  end
+
+  def get_kyujin(data_id)
+    list = get_kyujin_list
+    kyujin = list.select { |k| k["data_id"] == data_id }.first
+    pass = kyujin["link"]["href"]
+    res = RestClient.post(pass, @token)
+    Hash.from_xml(res.body)
+  end
+end
