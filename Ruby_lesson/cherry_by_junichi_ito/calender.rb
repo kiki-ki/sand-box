4badb71ec5965134758001d339664766a1700312
nakata kai <malpie0619@gmail.com>
2020-08-26 21:43:20 +0900

    reidai and add calender

---
+++ Ruby_lesson/cherry_by_junichi_ito/calender.rb
@@ -0,0 +1,23 @@
+require 'date'
+
+today = Date.today
+first_day_of_the_week = Date.new(today.year, today.month, 1).wday
+last_day = Date.new(today.year, today.month, -1).day
+
+header = today.strftime("%B %Y")
+week = %w[Su Mo Tu We Th Fr Sa]
+
+puts header
+puts week.join(" ")
+disp =  "   " * first_day_of_the_week
+
+(1..last_day).each do |day|
+  disp += day.to_s.rjust(2) + " "
+
+  if (first_day_of_the_week + day) % week.size == 0
+    puts disp
+    disp = ""
+  else
+    puts disp if day == last_day
+  end
+end
