3c16c04c477cee6dabe06aa7b78afc8bdc064268
nakata kai <malpie0619@gmail.com>
2020-09-06 20:32:43 +0900

    moved enigma dir to another rep

--- cipher/enigma/test.rb
+++
@@ -1,6 +0,0 @@
-require './enigma'
-
-enigma = Enigma.new(key_word: "XYZ")
-enigma.encrypt(code: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") # length = 30
-enigma.decrypt(encrypted_code: enigma.encrypted_code)
-p enigma
