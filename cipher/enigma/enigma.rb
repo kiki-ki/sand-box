87355c58ce9cab3408b7d7f4b3b46f1912c53dbd
nakata kai <malpie0619@gmail.com>
2020-09-06 18:06:42 +0900

    add message_key

--- cipher/enigma/enigma.rb
+++ cipher/enigma/enigma.rb
@@ -8,11 +8,12 @@ class Enigma
     attr_reader :code, :encrypted_code,
                 :plug_board, :roter1, :roter2, :roter3
 
-    def initialize(plug_patterns: {}, scrambler1: nil, scrambler2: nil, scrambler3: nil)
+    def initialize(key_word:, plug_patterns: {}, scrambler1: nil, scrambler2: nil, scrambler3: nil)
       @plug_board = PlugBoard.new(plug_patterns: plug_patterns)
-      @roter1 = Roter.new(scrambler: scrambler1)
-      @roter2 = Roter.new(scrambler: scrambler2)
-      @roter3 = Roter.new(scrambler: scrambler3)
+      message_keys = key_word.split("")
+      @roter1 = Roter.new(scrambler: scrambler1, message_key: message_keys[0])
+      @roter2 = Roter.new(scrambler: scrambler2, message_key: message_keys[1])
+      @roter3 = Roter.new(scrambler: scrambler3, message_key: message_keys[2])
     end
 
     def encrypt(code:)
@@ -22,18 +23,18 @@ class Enigma
       @encrypted_code = u_code.split('').map do |c|
         if c.match?(/[A-Z]/)
           key = plug_board.list.find_index(c)
-          roter1_key = roter1.rotated_scramble[key]
-          roter2_key = roter2.rotated_scramble[roter1_key]
-          roter3_key = roter3.rotated_scramble[roter2_key]
+          roter1_key = roter1.rotated_roter[key]
+          roter2_key = roter2.rotated_roter[roter1_key]
+          roter3_key = roter3.rotated_roter[roter2_key]
 
           ref_key = ((ALP_ARR.size - 1) - roter3_key)
-          roter3_ref_key = roter3.rotated_scramble.find_index(ref_key)
-          roter2_ref_key = roter2.rotated_scramble.find_index(roter3_ref_key)
-          roter1_ref_key = roter1.rotated_scramble.find_index(roter2_ref_key)
+          roter3_ref_key = roter3.rotated_roter.find_index(ref_key)
+          roter2_ref_key = roter2.rotated_roter.find_index(roter3_ref_key)
+          roter1_ref_key = roter1.rotated_roter.find_index(roter2_ref_key)
 
-          roter1.rotate
-          roter2.rotate if (roter1.rotation_cnt % 26).zero?
-          roter3.rotate if (roter2.rotation_cnt % 26).zero?
+          roter1.rotation
+          roter2.rotation if (roter1.rotation_cnt % 26).zero?
+          roter3.rotation if (roter2.rotation_cnt % 26).zero?
 
           plug_board.list[roter1_ref_key]
         else
@@ -44,11 +45,19 @@ class Enigma
   end
 
   class Decrypter
+    ALP_ARR = ("A".."Z").to_a.freeze
 
+    attr_reader :code, :encrypted_code,
+                :plug_board, :roter1, :roter2, :roter3
+
+    def initialize(plug_board:, scrambler1:, scrambler2:, scrambler3:)
+      @plug_board = plug_board
+      @scrambler1 = Roter.new
+    end
   end
 end
 
-encrypter = Enigma::Encrypter.new(plug_patterns: {J: "A"})
+encrypter = Enigma::Encrypter.new(key_word: "XYZ")
 p encrypter.plug_board
 p encrypter.roter1
 p encrypter.roter2
--- cipher/enigma/roter.rb
+++ cipher/enigma/roter.rb
@@ -1,32 +1,47 @@
 class Roter
   ALP_ARR = ("A".."Z").to_a.freeze
 
-  attr_reader :scrambler
+  attr_reader :scrambler, :message_key, :init_roter
   attr_accessor :rotation_cnt
 
-  def initialize(scrambler:)
+  def initialize(scrambler:, message_key:)
+    @message_key = message_key
+    validate_message_key!
+
     @scrambler = scrambler&.uniq || make_random_scrambler
-    @rotation_cnt = 0
     validate_scrambler!
+
+    @init_roter = make_init_roter
+    @rotation_cnt = 0
   end
 
-  def rotate
+  def rotation
     self.rotation_cnt += 1
   end
 
-  def rotated_scramble
-    scrambler.rotate(rotation_cnt)
+  def rotated_roter
+    init_roter.rotate(rotation_cnt)
   end
 
   private
 
+    def validate_message_key!
+      raise "message key is invalid. upcase alphabet values only." unless ("A".."Z").include?(@message_key)
+    end
+
     def validate_scrambler!
       raise "scrambler is invalid" if
-        scrambler.size != 26 ||
-        !scrambler.select { |v| !(0..(ALP_ARR.size - 1)).include?(v) }.empty?
+        @scrambler.size != 26 ||
+        !@scrambler.select { |v| !(0..(ALP_ARR.size - 1)).include?(v) }.empty?
     end
 
     def make_random_scrambler
       (0..(ALP_ARR.size - 1)).to_a.shuffle
+    end
+
+    def make_init_roter
+      idx = ALP_ARR.find_index(@message_key)
+      lead_idx = @scrambler.find_index(idx)
+      @scrambler.rotate(lead_idx)
     end
 end
